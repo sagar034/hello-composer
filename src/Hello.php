d1e96dcc749b40ab85f144c1890373099577dbb7
Sagar D. Satpute <ssatpute@dc2.propsol.org>
2020-07-12 12:54:57 +0530

    add more functions in hello class

--- src/Hello.php
+++ src/Hello.php
@@ -4,8 +4,20 @@ namespace samplecodetest\HelloComposer;
 
 class Hello
 {
+	private $m_strSting;
+
 	public function sayToDisplay($toSay = "Nothing given")
 	{
 		return $toSay;
+	}
+
+	public function sayToSet($strSting = "")
+	{
+		$this->m_strSting = $strSting;
+	}
+
+	public function displaySetValue($strSting = "")
+	{
+		return $this->m_strSting;
 	}
 }
